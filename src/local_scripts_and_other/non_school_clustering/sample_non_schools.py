ad20255abd678261fcd040618041645af4eaf2c6
Dominik Jambrović <dominik.jambrovic@gmail.com>
2024-10-23 20:18:39 +0200

    fix nonschool sampling

--- src/local_scripts_and_other/non_school_clustering/sample_non_schools.py
+++ src/local_scripts_and_other/non_school_clustering/sample_non_schools.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def calculate_euclidean_distance(point_df, point):
     # Transform geometry string to latitude, longitude tuple
@@ -46,15 +47,18 @@ if __name__ == "__main__":
     ### CENTROID 1
     # Calculate distance from centroid 1
     dataset_ns["c1_dist"] = dataset_ns["geometry"].apply(lambda row: calculate_euclidean_distance(row, centroid1))
-    
-    # Normalize the distances from centroid 1
-    min_distance1 = dataset_ns["c1_dist"].min()
-    max_distance1 = dataset_ns["c1_dist"].max()
-    dataset_ns["c1_norm_dist"] = (dataset_ns["c1_dist"] - min_distance1) / (max_distance1 - min_distance1)
 
     # Calculate probability to belong to centroid 1 (1 / d1)
-    dataset_ns["c1_prob"] = 1 / (dataset_ns["c1_norm_dist"] + 1e-10)
+    dataset_ns["c1_prob"] = 1 / (dataset_ns["c1_dist"] + 1e-10)
     dataset_ns["c1_prob"] = dataset_ns["c1_prob"] / dataset_ns["c1_prob"].sum()
+
+    # Plot sampling probability
+    plt.figure(figsize=(8, 6))
+    plt.title("Sampling Probability Depending on Distance to North Centroid")
+    plt.xlabel("Distance")
+    plt.ylabel("Probability")
+    plt.scatter(dataset_ns["c1_dist"], dataset_ns["c1_prob"])
+    plt.savefig(f"probability_plot_c1.png")
 
     # Choose nonschools for cluster 1 based on probability 1
     cluster1_ns_indices = np.random.choice(dataset_ns.index, size=len(cluster_1_rows), replace=False, p=dataset_ns["c1_prob"])
@@ -67,21 +71,24 @@ if __name__ == "__main__":
     # Calculate distance from centroid 2
     dataset_ns["c2_dist"] = dataset_ns["geometry"].apply(lambda row: calculate_euclidean_distance(row, centroid2))
 
-    # Normalize the distances from centroid 2
-    min_distance2 = dataset_ns["c2_dist"].min()
-    max_distance2 = dataset_ns["c2_dist"].max()
-    dataset_ns["c2_norm_dist"] = (dataset_ns["c2_dist"] - min_distance2) / (max_distance2 - min_distance2)
-
     # Calculate probability to belong to centroid 2 (1 / d2)
-    dataset_ns["c2_prob"] = 1 / (dataset_ns["c2_norm_dist"] + 1e-10)
+    dataset_ns["c2_prob"] = 1 / (dataset_ns["c2_dist"] + 1e-10)
     dataset_ns["c2_prob"] = dataset_ns["c2_prob"] / dataset_ns["c2_prob"].sum()
+
+    # Plot sampling probability
+    plt.figure(figsize=(8, 6))
+    plt.title("Sampling Probability Depending on Distance to South Centroid")
+    plt.xlabel("Distance")
+    plt.ylabel("Probability")
+    plt.scatter(dataset_ns["c2_dist"], dataset_ns["c2_prob"])
+    plt.savefig(f"probability_plot_c2.png")
 
     # Choose nonschools for cluster 2 based on probability 2
     cluster2_ns_indices = np.random.choice(dataset_ns.index, size=len(cluster_2_rows), replace=False, p=dataset_ns["c2_prob"])
     cluster2_ns = dataset_ns.loc[cluster2_ns_indices]
 
     # Drop columns related to centroid 1
-    cluster2_ns.drop(columns=["c1_dist", "c1_norm_dist", "c1_prob"], inplace=True)
+    cluster2_ns.drop(columns=["c1_dist", "c1_prob"], inplace=True)
 
     # Save selected non schools for each cluster
     cluster1_ns.to_csv(os.path.join(cwd, "non_schools_cluster_1.csv"), index=False)
