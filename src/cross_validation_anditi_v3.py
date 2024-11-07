62094657c2d39b79324858da353f045edb2bfe10
Dominik Jambrović <dominik.jambrovic@gmail.com>
2024-11-07 23:36:56 +0100

    plot schools, save csvs

--- src/cross_validation_anditi_v3.py
+++ src/cross_validation_anditi_v3.py
@@ -246,7 +246,11 @@ def plot_pr(preds_path, save_path):
     plt.legend()
     plt.savefig(os.path.join(save_path, "PR_val.png"))
 
-def plot_worst_ns(cwd, num_images=10):
+def plot_worst(cwd, anditi_dir, num_images=10):
+    save_dir = os.path.join(cwd, 'worst_examples')
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
     for i, file_name in enumerate(["train_preds1.csv", "train_preds2.csv"]):
         f = os.path.join(cwd, file_name)
         results = pd.read_csv(f)
@@ -254,11 +258,13 @@ def plot_worst_ns(cwd, num_images=10):
         # Set positive class probs
         results["y_probs_pos"] = results.apply(lambda row: row["y_probs"] if row["y_preds"] == 1 else 1 - row["y_probs"], axis=1)
 
+        ### Non-schools
         # Filter so that only non-schools are in dataframe
-        results = results[results["y_true"] == 0]
+        results_ns = results[results["y_true"] == 0]
 
         # Sort by positive class probs and take first N rows
-        ns_worst_rows = results.sort_values(by="y_probs_pos", ascending=False).head(num_images).reset_index(drop=True)
+        ns_worst_rows = results_ns.sort_values(by="y_probs_pos", ascending=False).head(num_images).reset_index(drop=True).to_crs('EPSG:4326')
+        ns_worst_rows.to_csv(os.path.join(save_dir, f"{num_images}_worst_ns_train_{i + 1}.csv"))
 
         plt.figure()
         plt.suptitle(f'Worst non-schools, train ({"North" if i == 0 else "South"})')
@@ -270,7 +276,27 @@ def plot_worst_ns(cwd, num_images=10):
             plt.imshow(image)
             plt.axis('off')
         plt.tight_layout()
-        plt.savefig(os.path.join(cwd, f"{num_images}_worst_ns_train_{i}.png"))
+        plt.savefig(os.path.join(save_dir, f"{num_images}_worst_ns_train_{i + 1}.png"))
+
+        ### Schools
+        # Filter so that only schools are in dataframe
+        results_s = results[results["y_true"] == 1]
+
+        # Sort by positive class probs and take first N rows
+        s_worst_rows = results_s.sort_values(by="y_probs_pos", ascending=True).head(num_images).reset_index(drop=True).to_crs('EPSG:4326')
+        s_worst_rows.to_csv(os.path.join(save_dir, f"{num_images}_worst_s_train_{i + 1}.csv"))
+
+        plt.figure()
+        plt.suptitle(f'Worst schools, train ({"North" if i == 0 else "South"})')
+        for j, row in s_worst_rows.iterrows():
+            path = f"{anditi_dir}/{row['UID']}.jpeg"
+            image = Image.open(path).convert("RGB")
+
+            plt.subplot(2, int(num_images / 2), j + 1)
+            plt.imshow(image)
+            plt.axis('off')
+        plt.tight_layout()
+        plt.savefig(os.path.join(save_dir, f"{num_images}_worst_s_train_{i + 1}.png"))
 
     for i, file_name in enumerate(["val_preds1.csv", "val_preds2.csv"]):
         f = os.path.join(cwd, file_name)
@@ -279,11 +305,13 @@ def plot_worst_ns(cwd, num_images=10):
         # Set positive class probs
         results["y_probs_pos"] = results.apply(lambda row: row["y_probs"] if row["y_preds"] == 1 else 1 - row["y_probs"], axis=1)
 
+        ### Non-schools
         # Filter so that only non-schools are in dataframe
-        results = results[results["y_true"] == 0]
+        results_ns = results[results["y_true"] == 0]
 
         # Sort by positive class probs and take first N rows
-        ns_worst_rows = results.sort_values(by="y_probs_pos", ascending=False).head(num_images).reset_index(drop=True)
+        ns_worst_rows = results_ns.sort_values(by="y_probs_pos", ascending=False).head(num_images).reset_index(drop=True).to_crs('EPSG:4326')
+        ns_worst_rows.to_csv(os.path.join(save_dir, f"{num_images}_worst_ns_val_{i + 1}.csv"))
 
         plt.figure()
         plt.suptitle(f'Worst non-schools, val ({"North -> South" if i == 0 else "South -> North"})')
@@ -295,7 +323,27 @@ def plot_worst_ns(cwd, num_images=10):
             plt.imshow(image)
             plt.axis('off')
         plt.tight_layout()
-        plt.savefig(os.path.join(cwd, f"{num_images}_worst_ns_val_{i}.png"))
+        plt.savefig(os.path.join(save_dir, f"{num_images}_worst_ns_val_{i + 1}.png"))
+
+        ### Schools
+        # Filter so that only schools are in dataframe
+        results_s = results[results["y_true"] == 1]
+
+        # Sort by positive class probs and take first N rows
+        s_worst_rows = results_s.sort_values(by="y_probs_pos", ascending=True).head(num_images).reset_index(drop=True).to_crs('EPSG:4326')
+        s_worst_rows.to_csv(os.path.join(save_dir, f"{num_images}_worst_s_val_{i + 1}.csv"))
+
+        plt.figure()
+        plt.suptitle(f'Worst schools, val ({"North -> South" if i == 0 else "South -> North"})')
+        for j, row in s_worst_rows.iterrows():
+            path = f"{anditi_dir}/{row['UID']}.jpeg"
+            image = Image.open(path).convert("RGB")
+
+            plt.subplot(2, int(num_images / 2), j + 1)
+            plt.imshow(image)
+            plt.axis('off')
+        plt.tight_layout()
+        plt.savefig(os.path.join(save_dir, f"{num_images}_worst_s_val_{i + 1}.png"))
 
 def main(c, exp_name="all", sampling="inverse", OHEM=False):    
     # f = "/mnt/sdb/agorup/school_mapping/inference_data/Anditi_filtered_schools_2-3857.csv"
@@ -894,7 +942,7 @@ def main(c, exp_name="all", sampling="inverse", OHEM=False):
     f.close()
 
     plot_pr(crossval_dir, crossval_dir)
-    plot_worst_ns(crossval_dir)
+    plot_worst(crossval_dir, dest_dir)
     
     # Terminate trackers
     time_elapsed = time.time() - since
