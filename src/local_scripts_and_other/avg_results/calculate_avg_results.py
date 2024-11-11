5222077d159d84101a3b87f0c16655cf7b6a0d93
Dominik Jambrović <dominik.jambrovic@gmail.com>
2024-11-11 13:01:52 +0100

    stddev

--- src/local_scripts_and_other/avg_results/calculate_avg_results.py
+++ src/local_scripts_and_other/avg_results/calculate_avg_results.py
@@ -1,5 +1,6 @@
 import os
 from collections import defaultdict
+import numpy as np
 
 base_dir = '../../../exp/cross_validation_anditi/'
 sampling_method = 'dynamic/gaussian'
@@ -26,8 +27,11 @@ for curr_run_name in os.listdir(runs_dir):
 
 with open(f'./{sampling_method.replace("/", "_")}_results_avg.txt', 'w') as avg_file:
     for i, (line, vals) in enumerate(results.items()):
-        avg_val = sum(vals) / len(vals)
-        avg_file.write(f"{line}: {avg_val}\n")
+        vals_np = np.array(vals)
+        avg = np.average(vals_np)
+        stddev = np.sqrt(np.var(vals_np))
+
+        avg_file.write(f"{line}: {avg.item():.3f} +- {stddev.item():.3f}\n")
 
         if i > 0 and (i + 1) % 3 == 0:
             avg_file.write("\n")
