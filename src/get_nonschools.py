3d1eced5ffd57b79a0707e736df4d69bcdfd1d42
Dominik Jambrović <dominik.jambrovic@gmail.com>
2025-01-13 12:34:00 +0100

    interval, info

--- src/get_nonschools.py
+++ src/get_nonschools.py
@@ -10,10 +10,18 @@ nonschools_list = pd.DataFrame(columns=['image'])
 for i, row in tiles.iterrows():
     image = f"{row['district']}-{row['index']}.jpeg"
 
-    if any((schools['lon'] == row['longitude']) & (schools['lat'] == row['latitude'])):
+    if any((schools['lon'] >= row['longitude'] - 155) & (schools['lon'] <= row['longitude'] + 155) & 
+           (schools['lat'] >= row['latitude'] - 155) & (schools['lat'] <= row['latitude'] + 155)):
         schools_list.loc[len(schools_list)] = {'image': image}
     else:
         nonschools_list.loc[len(nonschools_list)] = {'image': image}
 
 schools_list.to_csv(f'{base_path}/school_list.csv')
 nonschools_list.to_csv(f'{base_path}/nonschool_list.csv')
+
+print(f'Number of  schools: {len(schools_list.index)}')
+print(f'Number of  nonschools: {len(nonschools_list.index)}')
+print(f'Expected number of schools: {len(schools.index)}')
+
+print(f'Total: {len(schools_list.index) + len(nonschools_list.index)}')
+print(f'Expected total: {len(tiles.index)}')
